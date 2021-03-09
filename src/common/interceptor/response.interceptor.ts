f04944bbc182e2ae655b45719889fe54ab65c110
im6h <dangvu@qsoftvietnam.com>
2021-03-09 17:06:25 +0700

    :smile: Update interceptor

--- src/common/interceptor/response.interceptor.ts
+++ src/common/interceptor/response.interceptor.ts
@@ -1,12 +1,27 @@
-import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import {
+  CallHandler,
+  ExecutionContext,
+  Logger,
+  NestInterceptor,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { tap } from 'rxjs/operators';
+import { Request } from 'express';
 
 export class ResponseInterceptor implements NestInterceptor {
   intercept(
     context: ExecutionContext,
     next: CallHandler<any>,
   ): Observable<any> | Promise<Observable<any>> {
-    return next.handle().pipe(map((res) => res));
+    const req: Request = context.switchToHttp().getRequest();
+    const now = Date.now();
+    const { method, url, hostname } = req;
+    return next
+      .handle()
+      .pipe(
+        tap(() =>
+          Logger.verbose(`${hostname}-${method}-${url}-${Date.now() - now}ms`),
+        ),
+      );
   }
 }
